8ec347fe0f2433671e21461acf68f78af20b8b4d
Kevin Almeida <kevin.almeida1111.ka@gmail.com>
2024-03-17 21:32:53 +0200

    FEAT FIX
    In this point, the protocol was reformuled and the wireshark already can
    get the SYN.
    btw, the loopback return RST after unreplyed SYN/ACK, we need verify why
    the RST is sending automatic

--- main.c
+++ main.c
@@ -38,51 +38,38 @@ unsigned short csum(unsigned short *ptr, int nbytes) { // thank you AI 🤙
 
 int main(int argc, char* argv[]){
     int fd;
-    char packet[sizeof(struct ip) + sizeof(struct tcphdr)];
+    char packet[sizeof(struct tcphdr)];
+    memset(packet, 0, sizeof(struct tcphdr));
 
-    struct ip* ipHeader;
     struct tcphdr* tcpHeader;
     struct sockaddr_in addr;
     int one = 1;
 
-    fd = socket(AF_INET, SOCK_RAW,IPPROTO_RAW);
+    fd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
     if(fd == -1) error("Erro ao criar socket!");
+
 
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr(argv[1]);
 
-    //https://opensource.apple.com/source/xnu/xnu-344.34/bsd/netinet/ip.h.auto.html
-    ipHeader = (struct ip*)packet;
-    ipHeader->ip_hl = 5;
-    ipHeader->ip_v = 4;
-    ipHeader->ip_tos = 0;
-    ipHeader->ip_len = htons(sizeof(struct ip) + sizeof(struct tcphdr));
-    ipHeader->ip_id = 0;
-    ipHeader->ip_off = 0; 
-    ipHeader->ip_ttl = 0xff;
-    ipHeader->ip_p = IPPROTO_TCP;
-    ipHeader->ip_sum = 0;//will be calculate later
-    ipHeader->ip_src.s_addr = INADDR_ANY; //myself
-    ipHeader->ip_dst.s_addr = inet_addr(argv[1]);
 
     //https://opensource.apple.com/source/xnu/xnu-344.34/bsd/netinet/tcp.h.auto.html
     // yes agree that "th_*" is a shit name, complain to apple
-    tcpHeader = (struct tcphdr*)(packet + sizeof(struct ip));
-    //TODO try find best way to get the source port
-    tcpHeader->th_sport = htons(12345);
-    tcpHeader->th_dport = htons(atoi(argv[2])); // im not sure if can work in all cases because if uint16_t not signed int
-    tcpHeader->th_seq = 0;
-    tcpHeader->th_ack = 0;
-    tcpHeader->th_off = 5; //32 bits;
-    tcpHeader->th_flags = TH_SYN;
-    tcpHeader->th_win = htons(5840); //this value is not correcly, i just get a sample in wireshark haha :D
-    tcpHeader->th_sum = 0; // this will be calculate later
+     tcpHeader = (struct tcphdr*)packet;
+     //TODO try find best way to get the source port
+     tcpHeader->th_sport = htons(12345);
+     tcpHeader->th_dport = htons(atoi(argv[2])); // im not sure if can work in all cases because if uint16_t not signed int
+     tcpHeader->th_seq = 0;
+     tcpHeader->th_ack = 0;
+     tcpHeader->th_off = 5; //32 bits;
+     tcpHeader->th_flags = TH_SYN;
+     tcpHeader->th_win = htons(5840); //this value is not correcly, i just get a sample in wireshark haha :D
+     tcpHeader->th_sum = 0; // this will be calculate later
+ 
+     //calculate the checksum
+     tcpHeader->th_sum = csum((unsigned short*)packet, sizeof(struct tcphdr));
 
-    //calculate the checksum
-    ipHeader -> ip_sum = csum((unsigned short*)packet, sizeof(struct ip));
-    tcpHeader->th_sum = csum((unsigned short*)(packet + sizeof(struct ip)), sizeof(struct tcphdr));
-
-    int ipack = sendto(fd, &packet,(sizeof(struct ip) + sizeof(struct tcphdr)), 0, (struct sockaddr*)&addr, sizeof(addr));
+    int ipack = sendto(fd, packet,(sizeof(struct ip) + sizeof(struct tcphdr)), 0, (struct sockaddr*)&addr, sizeof(addr));
 
     if(ipack == -1){
         error("Erro ao enviar o pacote SYN!");
