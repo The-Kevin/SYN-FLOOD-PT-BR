111b4ed8d6c89d504b2fee40609165010e7648a4
Kevin Almeida <kevin.almeida1111.ka@gmail.com>
2024-03-17 13:34:17 +0200

    The IP protocol header was be mounted manually, a option in socket was configured to get the manual headers, all struct is working right but the sendto is returning "invalid args", we need investigate more...

--- main.c
+++ main.c
@@ -8,53 +8,91 @@
 #include <arpa/inet.h>
 
 void error(char* msg) {
-  perror(("Error: %s\n", msg));
+  perror(("\nError: %s\n", msg));
   exit(EXIT_FAILURE);
 }
 
-//ONLY APPLE
-void defineTCPHeader(struct tcphdr* tcp, uint16_t port, unsigned short checksum){
-    //https://opensource.apple.com/source/xnu/xnu-344.34/bsd/netinet/tcp.h.auto.html
-    //
-    //verify a best way to get source port from the kernel
-    unsigned short sourcePort = 50689;
-    tcp->th_ack = 0;
-    tcp->th_sport = sourcePort;
-    tcp->th_dport = port;
-    tcp->th_flags = TH_SYN; 
-    tcp->th_off = sizeof(struct tcphdr) / 4; // Tamanho do cabeçalho TCP em  32 bits
-    tcp->th_seq = 0;
-    tcp->th_sum = checksum;
-    tcp->th_urp = 0;
-    tcp->th_win = htons(65535); // this value was getted from wireshark sniff by empirically experience haha
+unsigned short csum(unsigned short *ptr, int nbytes) { // thank you AI 🤙
+    register long sum;
+    unsigned short oddbyte;
+    register short answer;
+
+    sum = 0;
+    while (nbytes > 1) {
+        sum += *ptr++;
+        nbytes -= 2;
+    }
+
+    if (nbytes == 1) {
+        oddbyte = 0;
+        *((u_char *)&oddbyte) = *(u_char *)ptr;
+        sum += oddbyte;
+    }
+
+    sum = (sum >> 16) + (sum & 0xffff);
+    sum += (sum >> 16);
+    answer = (short)~sum;
+
+    return answer;
 }
 
 int main(int argc, char* argv[]){
-
     int fd;
-    struct sockaddr_in addr;
-    struct tcphdr *tcpHeader;
-    fd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
-    if(fd == -1){
-        error("Erro! Impossivel criar socket de conexao!");
-    }
+    char packet[sizeof(struct ip) + sizeof(struct tcphdr)];
 
+    struct ip* ipHeader;
+    struct tcphdr* tcpHeader;
+    struct sockaddr_in addr;
+    int one = 1;
+
+    fd = socket(AF_INET, SOCK_RAW,IPPROTO_RAW);
+    if(fd == -1) error("Erro ao criar socket!");
+
+    //https://opensource.apple.com/source/xnu/xnu-344.34/bsd/netinet/in.h.auto.html
+    if(setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) == - 1){
+        error("Erro ao configurar opcoes no socket!");
+    } 
 
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr(argv[1]);
-    addr.sin_port = htons(atoi(argv[2]));                                     
-    defineTCPHeader(tcpHeader, atoi(argv[2]), 0);                                   
 
-    int pack = sendto(fd, tcpHeader, sizeof(struct tcphdr), 0, (struct sockaddr*)&addr, sizeof(addr));
+    //https://opensource.apple.com/source/xnu/xnu-344.34/bsd/netinet/ip.h.auto.html
+    ipHeader = (struct ip*)packet;
+    ipHeader->ip_hl = 5;
+    ipHeader->ip_v = 4;
+    ipHeader->ip_tos = 0;
+    ipHeader->ip_len = htons(sizeof(struct ip) + sizeof(struct tcphdr));
+    ipHeader->ip_id = 0;
+    ipHeader->ip_off = 0; 
+    ipHeader->ip_ttl = 0xff;
+    ipHeader->ip_p = IPPROTO_TCP;
+    ipHeader->ip_sum = 0;//will be calculate later
+    ipHeader->ip_src.s_addr = inet_addr("192.168.8.153"); //myself
+    ipHeader->ip_dst.s_addr = inet_addr(argv[1]);
 
-   if(pack == -1){
-        error("Erro: Nao foi possivel enviar o SYN!");
-   } 
+    //https://opensource.apple.com/source/xnu/xnu-344.34/bsd/netinet/tcp.h.auto.html
+    // yes agree that "th_*" is a shit name, complain to apple
+    tcpHeader = (struct tcphdr*)(packet + sizeof(struct ip));
+    //TODO try find best way to get the source port
+    tcpHeader->th_sport = htons(12345);
+    tcpHeader->th_dport = htons(atoi(argv[2])); // im not sure if can work in all cases because if uint16_t not signed int
+    tcpHeader->th_seq = 0;
+    tcpHeader->th_ack = 0;
+    tcpHeader->th_off = 5; //32 bits;
+    tcpHeader->th_flags = TH_SYN;
+    tcpHeader->th_win = htons(5840); //this value is not correcly, i just get a sample in wireshark haha :D
+    tcpHeader->th_sum = 0; // this will be calculate later
 
-   printf("Pacote SYN enviado com sucesso!");
+    //calculate the checksum
+    ipHeader -> ip_sum = csum((unsigned short*)packet, sizeof(struct ip));
+    tcpHeader->th_sum = csum((unsigned short*)(packet + sizeof(struct ip)), sizeof(struct tcphdr));
+
+    int ipack = sendto(fd, &packet,(sizeof(struct ip) + sizeof(struct tcphdr)), 0, (struct sockaddr*)&addr, sizeof(addr));
+
+    if(ipack == -1){
+        error("Erro ao enviar o pacote SYN!");
+    }
     
     close(fd);
-
-    
     return 0;
 }
